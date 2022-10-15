a025db98da4c47eca005ec85d0d7880e5a87f1dc
amanchy <aman@portpro.io>
2022-10-15 13:52:48 +0545

    Add: dashboardSection

--- frontend/src/Screens/Dashboard/DashboardSection2.js
+++ frontend/src/Screens/Dashboard/DashboardSection2.js
@@ -2,8 +2,10 @@ import React from 'react'
 
 export const DashboardSection2 =()=>{
     return (
-        <div className="dashboard__section2">
-            Dashboard section 2
+        <div className="dashboard__section2 col-7">
+            <div className="dashboard__section2__content">
+                Dashboard Section 2
+            </div>
         </div>
     )
 }
