1b14f34decae6e2fe97e6ee258e721024d994f3b
amanchy <aman@portpro.io>
2022-10-15 09:50:00 +0545

    Add: TabContent file

--- frontend/src/Screens/TabContent.js
+++ frontend/src/Screens/TabContent.js
@@ -1,11 +1,14 @@
 import React from 'react';
 
 import "../styles/screens/home.css"
+import { DashboardContainer } from './Dashboard/DashboardContainer';
 
 export const TabContent =()=>{
     return(
         <div className="tab__content">
-            Tab Content
+            <div className="tab__content__inside">
+                <DashboardContainer/>
+            </div>
         </div>
     )
 }
