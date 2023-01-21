cdbf52f154b126b2d3f0bdbec6b0327b03c7a356
amanc1248 <amanc1248@gmail.com>
2023-01-21 19:21:52 +0530

    Fix: duplicate tutor list

--- frontend/src/utils/utils.js
+++ frontend/src/utils/utils.js
@@ -41,7 +41,38 @@ function removeDuplicates(arr, n) {
   }
   return list;
 }
+
+function removeDuplicatesWithFieldName(arr, n,filedName) {
+  // Return, if array is empty
+  // or contains a single element
+  if (n === 0 || n === 1) return arr;
+
+  var temp = new Array(n);
+
+  // Start traversing elements
+  var j = 0;
+  for (var i = 0; i < n - 1; i++)
+    // If current element is not equal
+    // to next element then store that
+    // current element
+    if (arr[i][filedName] !== arr[i + 1][filedName]) temp[j++] = arr[i];
+
+  // Store the last element as whether
+  // it is unique or repeated, it hasn't
+  // stored previously
+  temp[j++] = arr[n - 1];
+
+  // Modify original array
+  for (var i = 0; i < j; i++) arr[i] = temp[i];
+
+  const list = [];
+  for (var i=0; i<j; i++){
+    list.push(arr[i])
+  }
+  return list;
+}
 module.exports = {
   generateSearchOptions,
   removeDuplicates,
+  removeDuplicatesWithFieldName
 };
